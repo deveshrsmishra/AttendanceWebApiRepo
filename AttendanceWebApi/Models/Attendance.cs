aa66dfca753349f19804b5da7c1a0f92fb0b2d14
deveshrsmishra <105710654+deveshrsmishra@users.noreply.github.com>
2022-05-18 16:41:30 +0530

    Add files via upload

---
+++ AttendanceWebApi/Models/Attendance.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AttendanceWebApi.Models
+{
+    public class Attendance
+    {
+        [Key]
+        public string AttendanceId { get; set; }
+
+        public string AttendanceStatus { get; set; }
+
+        public string AttendanceDate { get; set; }
+    }
+}
